1f94abb1d6587b67fe16f575e7a211a61d3523a3
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-02-16 14:42:50 +0100

    done es 2 e es 2/b

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,30 @@ Dichiara una variabile chiamata number e assegnale il valore 100. Stampa il valo
 const number = 100;
 
 console.log(number);
+
+/*Esercizio 2
+Dichiara una costante chiamata PI e assegnale il valore di 3.14. Stampa il valore della costante in console.*/
+
+const PI = 3.14;
+
+console.log(PI);
+
+/*Esercizio 2/B
+Prova ad assegnare alla costante chiamata PI il valore di 3.1416. Sai dire cosa succede?*/
+    /*Risposta: main.js:20 Uncaught TypeError: Assignment to constant variable.
+    NON possiamo cambiare il valore di una variabile COSTANTE (const)
+    Diverso il discorso con una variabile di tipo 'let' o 'var' (segue esempio con let PIgreco) */
+
+//PI = 3.1416;
+
+console.log(PI);
+
+//Possiamo farlo con let
+
+let PIgreco = 3.14;
+
+console.log(PIgreco);
+
+PIgreco = 3.1416;
+
+console.log(PIgreco);
--- assets/js/readme.md
+++ assets/js/readme.md
@@ -7,3 +7,15 @@ Dichiara una variabile chiamata number e assegnale il valore 100. Stampa il valo
 
 
 
+Esercizio 2
+Dichiara una costante chiamata PI e assegnale il valore di 3.14. Stampa il valore della costante in console.
+
+- Dichiaro la variabile
+- Assegno alla variabile il valore 3.14 con l' = (specifico const)
+- Chiamo la variabile con la funzione log
+
+Esercizio 2/B
+Prova ad assegnare alla costante chiamata PI il valore di 3.1416. Sai dire cosa succede?
+
+- Provo a cambiare il contenuto alla variabile il valore 3.1416 con l' =
+- Non funzionerà perché è una costante
